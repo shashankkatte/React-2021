365f813444fc3e6db5b650a55acf17cd6af92907
shashankkatte <shashankanand.katte@gmail.com>
2021-02-02 12:54:18 -0500

    src : Converting HTML to JSX
    Demo of how to convert normal HTML to JSX.
    Things like style gets converted to JS objects, and hyphenated properties need to be changed to camel case

--- hello-jsx/src/index.js
+++ hello-jsx/src/index.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create the Reacy components
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'white'}}> submit</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
