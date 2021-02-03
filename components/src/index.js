da24dd6f62479c7baab42bd56a82a65f78487bd1
shashankkatte <shashankanand.katte@gmail.com>
2021-02-03 11:55:18 -0500

    src : Component nesting
    Demo of nesting compnents, refactored comments div intto a seperate component CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-            <a href="/" className="author">
-                Samuel
-            </a>
-            <div className="metadata">
-                <span className="date"> Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-            <a href="/" className="author">
-                Samuel
-            </a>
-            <div className="metadata">
-                <span className="date"> Today at 6:30PM</span>
-            </div>
-            <div className="text">Nice blog again</div>
-        </div>
-      </div>
-    </div>
+       <div className="ui container comments">
+           <CommentDetail />
+       </div>
   );
 };
 
