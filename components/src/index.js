d83b042ab2080cea5d400baf60c279f4c24fb906
shashankkatte <shashankanand.katte@gmail.com>
2021-02-03 13:55:03 -0500

    src : Demo of multiple props

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
-       <div className="ui container comments">
-           <CommentDetail author="Samuel"/>
-           <CommentDetail author="Tom"/>
-           <CommentDetail author="SamRajuel"/>
-       </div>
+    <div className="ui container comments">
+      <CommentDetail author="Samuel" timeAgo="Today at 15.34 PM" avatar = {faker.image.image()} comment="Nice blog" />
+      <CommentDetail author="Tom" timeAgo="Today at 5.34 AM" avatar = {faker.image.image()} comment="good opinion"/>
+      <CommentDetail author="SamRajuel" timeAgo="Today at 13.34 PM" avatar = {faker.image.image()} comment="where's the proof?" />
+    </div>
   );
 };
 
