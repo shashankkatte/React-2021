9f5a76a489dfe2902592ecac463a6d02dcf8bb5c
shashankkatte <shashankanand.katte@gmail.com>
2021-02-03 14:18:33 -0500

    src : Demo of child component using semantic UI cards

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Samuel" timeAgo="Today at 15.34 PM" avatar = {faker.image.image()} comment="Nice blog" />
-      <CommentDetail author="Tom" timeAgo="Today at 5.34 AM" avatar = {faker.image.image()} comment="good opinion"/>
-      <CommentDetail author="SamRajuel" timeAgo="Today at 13.34 PM" avatar = {faker.image.image()} comment="where's the proof?" />
+      <ApprovalCard>
+        <CommentDetail
+          author="Samuel"
+          timeAgo="Today at 15.34 PM"
+          avatar={faker.image.image()}
+          comment="Nice blog"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Tom"
+          timeAgo="Today at 5.34 AM"
+          avatar={faker.image.image()}
+          comment="good opinion"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="SamRajuel"
+          timeAgo="Today at 13.34 PM"
+          avatar={faker.image.image()}
+          comment="where's the proof?"
+        />
+      </ApprovalCard>
     </div>
   );
 };
